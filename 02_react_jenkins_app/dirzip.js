330325e8f7caad53fe9b25efae45478dedd1f8ff
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2025-09-21 17:18:04 +0530

    modified zip automation

--- 02_react_jenkins_app/dirzip.js
+++ 02_react_jenkins_app/dirzip.js
@@ -1,21 +1,28 @@
-import path from "path";
-import { exec } from "child_process";
+import fs from "fs"
+import archiver from "archiver";
 
-const directoryPathToZip = 'dist'; // Replace with the actual directory to zip
-const zipFileName = 'dist.zip'; // Desired name for the zip file
-
-// Construct the command to zip the directory recursively
-// The '-r' flag ensures recursive zipping of the directory's contents
-const command = `zip -r ${zipFileName} ${directoryPathToZip}`;
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error zipping directory: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`Directory "${directoryPathToZip}" successfully zipped to "${zipFileName}".`);
+// The output file path
+const output = fs.createWriteStream(__dirname + '/dist.zip');
+const archive = archiver('zip', {
+zlib: { level: 9 } // Sets the compression level
 });
+
+// Listen for all archive data to be written
+output.on('close', function() {
+console.log(archive.pointer() + ' total bytes');
+console.log('Archiver has been finalized and the output file descriptor has closed.');
+});
+
+// Handle errors
+archive.on('error', function(err) {
+throw err;
+});
+
+// Pipe archive data to the file
+archive.pipe(output);
+
+// Append the entire 'build' directory
+archive.directory('build/', false);
+
+// Finalize the archive (this will close the output stream)
+archive.finalize();
